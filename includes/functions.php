2ff37fb5db9b5f855b50581b0cab59f998a8d56b
Luiz Bills <luizpbills@gmail.com>
2018-06-08 16:18:07 +0000

    initial commit

---
+++ includes/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+use Timber\Loader;
+
+function get_twig_template ( $path , $data = [] ) {
+	$context = $context = array_merge( Timber::get_context(), $data );
+	$cache = get_twig_cache_settings( $path );
+	return Timber::compile( $path, $context, $cache['expires'], $cache['cache_mode'] );
+}
+
+function twig_template ( $path, $data = [] ) {
+	echo get_twig_template( $path, $data );
+}
+
+function twig_template_callback ( $path, $data = [] ) {
+	return function () use ( $path, $data ) {
+		$output = get_twig_template( $path, $data );
+		echo $output;
+	};
+}
+
+function get_twig_cache_settings ( $path ) {
+	return apply_filters( 'twig_helpers_cache_settings', [
+		'expires' => false,
+		'cache_mode' => Loader::CACHE_USE_DEFAULT
+	], $path );
+}
---
+++ twig-helpers.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Twig Helpers
+Plugin URI: https://github.com/luizbills/wp-twig-helpers
+Description: Introduces helper functions to easy and fast theme development with Twig
+Version: 1.0.0
+Author: Luiz Bills
+Author URI: https://luizpb.com/en
+Text Domain: twig-helpers
+Domain Path: /languages
+*/
+
+if ( ! defined( 'WPINC' ) ) die();
+
+if ( ! class_exists( 'Twig_Helpers' ) ) :
+
+class Twig_Helpers {
+
+	const VERSION = '1.0.0';
+	const FILE = __FILE__;
+	const DIR = __DIR__;
+	const PREFIX = 'twig_helpers_';
+
+	protected static $_instance = null;
+
+	protected function __construct () {
+		if ( ! defined( 'TIMBER_LOADED' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice' ] );
+			return;
+		}
+
+		$this->includes();
+	}
+
+	protected function includes () {
+		require_once __DIR__ . '/includes/functions.php';
+	}
+
+	public function admin_notice () {
+		$message = sprintf(
+			__( 'Please, install %s plugin.', 'wp-helpers' ),
+			'<a target="_blank" href="https://wordpress.org/plugins/timber-library/">Timber</a>'
+		); ?>
+		<div class="notice notice-error is-dismissible">
+			<p><?php echo $message; ?></p>
+		</div>
+		<?php
+	}
+
+	public static function get_instance () {
+		if ( null === self::$_instance ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+}
+
+function Twig_Helpers () {
+	return Twig_Helpers::get_instance();
+}
+
+add_action( 'plugins_loaded', 'Twig_Helpers' );
+
+endif;
