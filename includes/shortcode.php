5017f8932c04c1cec60c74333bd9ffda87209fe3
Luiz Bills <luizpbills@gmail.com>
2018-07-23 19:35:29 +0000

    add shortcode

---
+++ includes/shortcode.php
@@ -0,0 +1,28 @@
+<?php
+
+add_shortcode( 'twig_template', function ( $args, $content = '' ) {
+    $defaults = [
+        'path' => ''
+    ];
+    $args = array_merge( $defaults, $args );
+    $path = $args['path'];
+    $data = [];
+
+    unset( $args['path'] );
+
+    foreach ( $args as $key => $value ) {
+        $data[ $key ] = $value;
+    }
+    $data['content'] = $content;
+
+    $result = compile_twig_template( $path, $data, false );
+
+    if ( false === $result ) {
+        throw new Exception( 'Invalid path in twig_template shortcode' );
+    }
+
+    $result = apply_filters( 'twig_helpers_before_template_' . $path, '', $data, $content ) . $result;
+    $result .= apply_filters( 'twig_helpers_after_template_' . $path, '', $data, $content );
+
+    return $result;
+} );
